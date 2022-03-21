b1366ff640666b98ed5eb4a31fbeea05d95758cf
anatoliy0707 <anatoliy_sukhanov@mail.ru>
2022-03-21 16:58:27 +0500

    fix MyPosts, added pureComponent extends, finished 87

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,23 +5,30 @@ import {MyPostsPropsType} from "./MyPostsContainer";
 import {AddPostReduxForm, FormDataType} from "./AddPostForm/AddPostForm";
 
 
-function MyPosts(props: MyPostsPropsType) {
-    const postsElement = props.posts.map((post) => (
-        <Post key={post.id} message={post.message} likesCounter={post.likesCounter}/>
-    ));
+class MyPosts extends React.PureComponent<MyPostsPropsType> {
+
+    // shouldComponentUpdate(nextProps: Readonly<MyPostsPropsType>, nextState: Readonly<{}>): boolean {
+    //     return nextProps !== this.props || nextState !== this.state
+    // }
+
+    render() {
+        const postsElement = this.props.posts.map((post) => (
+            <Post key={post.id} message={post.message} likesCounter={post.likesCounter}/>
+        ));
 
 
-    const addPostHandler = (formData: FormDataType) => {
-        props.addPost(formData.postMessage)
+        const addPostHandler = (formData: FormDataType) => {
+            this.props.addPost(formData.postMessage)
+        }
+
+        return (
+            <div className={s.postsBlock}>
+                <h3>My posts</h3>
+                <AddPostReduxForm onSubmit={addPostHandler}/>
+                <div className={s.posts}>{postsElement}</div>
+            </div>
+        );
     }
-
-    return (
-        <div className={s.postsBlock}>
-            <h3>My posts</h3>
-            <AddPostReduxForm onSubmit={addPostHandler}/>
-            <div className={s.posts}>{postsElement}</div>
-        </div>
-    );
 }
 
 export default MyPosts;
